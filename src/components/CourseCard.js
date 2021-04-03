2b2e9c66b813485bf67c1c26e96493402788bec1
PlaneCargo191 <t.james_17@hotmail.com>
2021-04-03 16:49:52 +0700

    1.0

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,4 +1,21 @@
 export const CourseCard = (props) => {
   // TODO: design HTML component that displays a course as a "card" on the webpage.
-  return (null);
+  return (<>
+    <div class = "CourseCard">
+      <tr id = "CC">
+        <td>
+          <div class = "tag">
+            <p>Subject: {props.subj}</p>
+            <p>GPA: {props.grade}</p>
+            <p>Credit: {props.credit}</p>
+          </div>
+        </td>
+        <td>
+          <button class = "btn" onClick = {() => {
+              props.del(props.subj)
+          }}> X </button>
+        </td>
+      </tr>
+    </div>
+  </>);
 };
